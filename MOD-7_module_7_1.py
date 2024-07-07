f18ed02176cecbe72580b7644917026c3aaf8b05
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-07-07 23:54:55 +0300

    Update MOD-7_module_7_1.py

--- MOD-7_module_7_1.py
+++ MOD-7_module_7_1.py
@@ -37,8 +37,12 @@ p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p2)  # __str__
+print(p2)  
 
 s1.add(p1, p2, p3)
+
+
+
+
 
 print(s1.get_products())
