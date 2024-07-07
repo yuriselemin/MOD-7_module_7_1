78f97e2ac08785996719cce8f2850e12449baaf1
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-07-08 00:24:01 +0300

    Update MOD-7_module_7_1.py

--- MOD-7_module_7_1.py
+++ MOD-7_module_7_1.py
@@ -1,4 +1,3 @@
-
 # Домашнее задание по теме "Режимы открытия файлов"
 
 class Product:
@@ -15,34 +14,30 @@ class Shop:
         self.__file_name = 'products.txt'
 
     def get_products(self):
-        with open(self.__file_name, 'r') as file:
-            products = file.readlines()
+        file = open(self.__file_name, 'r')
+        products = file.readlines()
         file.close()
         return ''.join(products)
 
     def add(self, *products):
         added_products = []
-        with open(self.__file_name, 'a') as file:
-            for product in products:
-                if product.name not in [item.name for item in added_products]:
-                    file.write(str(product) + '\n')
-                    added_products.append(product)
-                else:
-                    print(f'Продукт {product.name} уже есть в магазине')
+        file = open(self.__file_name, 'a')
+        for product in products:
+            if product.name not in [item.name for item in added_products]:
+                file.write(str(product) + '\n')
+                added_products.append(product)
+            else:
+                print(f'Продукт {product.name} уже есть в магазине')
         file.close()
 
-# Пример использования:
+
 s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p2)  
+print(p2)  # __str__
 
 s1.add(p1, p2, p3)
-
-
-
-
 
 print(s1.get_products())
